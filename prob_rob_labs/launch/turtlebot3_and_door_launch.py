618c0b15439a1da3bf476069c0c7f10740227c29
Ilija Hadzic <ih2435@columbia.edu>
2026-06-06 20:46:38 -0400

    labs: refactor door and firetruck launch files
    include the lower-level launch file instead of crafting
    own instantiation of turtlebot

--- prob_rob_labs/launch/turtlebot3_and_door_launch.py
+++ prob_rob_labs/launch/turtlebot3_and_door_launch.py
@@ -29,28 +29,13 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
 from launch.actions import DeclareLaunchArgument
-from launch.actions import AppendEnvironmentVariable
-from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
 from launch.conditions import IfCondition
 
 def generate_launch_description():
-    tb3_launch_dir = os.path.join(get_package_share_directory(
-        'turtlebot3_gazebo'), 'launch')
-    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
-
-    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    world = LaunchConfiguration('world', default='door.world')
     x_pose = LaunchConfiguration('x_pose', default='-1.5')
     y_pose = LaunchConfiguration('y_pose', default='0.0')
-
-    world = LaunchConfiguration('world', default='door.world')
-
-    world_path = PathJoinSubstitution([
-        FindPackageShare('prob_rob_labs'),
-        'worlds',
-        world
-    ])
 
     run_door_opener = LaunchConfiguration('run_door_opener', default='false')
 
@@ -74,38 +59,13 @@ def generate_launch_description():
         description='Whether to run the vision processing nodes'
     )
 
-    gzserver_cmd = IncludeLaunchDescription(
+    turtlebot3_world_launch_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')
+            os.path.join(get_package_share_directory('prob_rob_labs'),
+                         'launch', 'turtlebot3_world_launch.py')
         ),
         launch_arguments={
-            'gz_args': ['-r -s -v4 ', world_path], 'on_exit_shutdown': 'true'}.items()
-    )
-
-    gzclient_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')
-        ),
-        launch_arguments={'gz_args': '-g -v4 '}.items()
-    )
-
-    set_env_vars_resources = AppendEnvironmentVariable(
-        'GZ_SIM_RESOURCE_PATH',
-        os.path.join(get_package_share_directory('turtlebot3_gazebo'),
-                     'models'))
-
-    robot_state_publisher_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(tb3_launch_dir, 'robot_state_publisher.launch.py')
-        ),
-        launch_arguments={'use_sim_time': use_sim_time}.items()
-    )
-
-    spawn_turtlebot_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(tb3_launch_dir, 'spawn_turtlebot3.launch.py')
-        ),
-        launch_arguments={
+            'world': world,
             'x_pose': x_pose,
             'y_pose': y_pose
         }.items()
@@ -144,14 +104,10 @@ def generate_launch_description():
 
     ld = LaunchDescription()
 
-    ld.add_action(set_env_vars_resources)
     ld.add_action(declare_world_arg)
     ld.add_action(declare_run_door_opener_arg)
     ld.add_action(declare_run_vision_processor_arg)
-    ld.add_action(gzserver_cmd)
-    ld.add_action(gzclient_cmd)
-    ld.add_action(robot_state_publisher_cmd)
-    ld.add_action(spawn_turtlebot_cmd)
+    ld.add_action(turtlebot3_world_launch_cmd)
     ld.add_action(door_torque_bridge_cmd)
     ld.add_action(flaky_door_opener_cmd)
     ld.add_action(video_processor_cmd)
